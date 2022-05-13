974ff2d10b1947d6553c0f4dea680871d87d1003
Barry de Graaff <4353213+barrydegraaff@users.noreply.github.com>
2022-05-13 12:23:28 +0200

    implement feedback

--- src/com/zimbra/customauthguide/customAuthGuideAuthHandler.java
+++ src/com/zimbra/customauthguide/customAuthGuideAuthHandler.java
@@ -1,9 +1,13 @@
 package com.zimbra.customauthguide;
 
 import java.util.*;
+
+import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.AccountServiceException;
 import com.zimbra.cs.account.auth.ZimbraCustomAuth;
+import com.zimbra.cs.listeners.AuthListener;
 
 public class customAuthGuideAuthHandler extends ZimbraCustomAuth {
     public void register(String id) {
@@ -29,20 +33,33 @@ public class customAuthGuideAuthHandler extends ZimbraCustomAuth {
      *
      */
     @Override
-    public void authenticate(Account account, String password, Map<String, Object> context, List<String> args) throws Exception {
+    public void authenticate(Account account, String password, Map<String, Object> context, List<String> args) throws ServiceException {
+        if (!isAuthenticated(account, password, context)) {
+            AccountServiceException.AuthFailedServiceException afse = AccountServiceException.AuthFailedServiceException.AUTH_FAILED("customAuthGuide Authentication failed");
+            AuthListener.invokeOnException(afse);
+            throw afse;
+        }
+    }
+
+    /*
+    * Implement log-in validation here
+    * */
+    protected boolean isAuthenticated(Account account, String password, Map<String, Object> context) {
+        //THIS IS JUST AN EXAMPLE, NEVER HARDCODE USERNAMES, PASSWORDS AND IP'S THIS WAY!!
         if (("testuser@example.com".equals(account.getName())) && ("test123".equals(password))) {
             if ("imap".equals(context.get("proto").toString())) {
-                throw new Exception("customAuthGuide Authentication failed, IMAP is not permitted for this user");
+                ZimbraLog.account.warn("customAuthGuide Authentication failed, IMAP is not permitted for this user %s", account.getName());
+                return false;
             }
 
             //to make this work make sure to read https://wiki.zimbra.com/wiki/Secopstips#Log_the_correct_origination_IP
             if ("54.83.74.191".equals(context.get("ocip"))) {
-                throw new Exception("customAuthGuide Authentication failed, IP is not permitted for this user");
+                ZimbraLog.account.warn("customAuthGuide Authentication failed, IP is not permitted for this user %s", account.getName());
+                return false;
             }
-
             ZimbraLog.account.info("customAuthGuide Authentication success %s", account.getName());
-            return;
+            return true; //only return true if authentication has succeeded!
         }
-        throw new Exception("customAuthGuide Authentication failed");
+        return false;
     }
 }
